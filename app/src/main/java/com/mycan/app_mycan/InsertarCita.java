5173ef13ea743d1cddfb159ee3282c1036c579d6
Nono <nono_20491@hotmail.com>
2015-04-17 14:16:21 +0200

    Probando el control de fecha y hora

--- app/src/main/java/com/mycan/app_mycan/InsertarCita.java
+++ app/src/main/java/com/mycan/app_mycan/InsertarCita.java
@@ -123,17 +123,74 @@ public class InsertarCita extends ActionBarActivity {
     }
 
     public void onClick(View v) {
+        boolean flag = false;
         if (v.getId() == R.id.btGuardarCita) {
-            if (modo.equalsIgnoreCase("insertar") || modo.equalsIgnoreCase("menulistcitas")) {
-                insertarCita();
-            } else if (modo.equalsIgnoreCase("modificar")) {
-                modificarCita();
+            if (comprobarDatos()) {
+                if (modo.equalsIgnoreCase("insertar") || modo.equalsIgnoreCase("menulistcitas")) {
+                    insertarCita();
+                } else if (modo.equalsIgnoreCase("modificar")) {
+                    modificarCita();
+                }
+
+            } else if (v.getId() == R.id.btVolverCita) {
+                finish();
             }
-        } else if (v.getId() == R.id.btVolverCita) {
-            finish();
         }
     }
 
+    public void insertarCita() {
+        boolean flag = false;
+
+        // PREPARO LA INSERCION
+        adbCitas = new AdaptadorDBCitas(ctx);
+        cv = new ContentValues();
+        cv.put("fecha", fecha);
+        cv.put("hora", hora);
+        cv.put("precio", precio);
+        cv.put("descripcion", descripcion);
+        cv.put("id_mascota", id_mascota);
+        // INSERTO LA CITA (TRUE -> OK | FALSE -> ERROR AL INSERTAR)
+        try {
+            adbCitas.abrirConexion();
+            flag = adbCitas.insertarCita(cv);
+            adbCitas.cerrarConexion();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // COMPRUEBO QUE LA INSERCION HA SIDO CORRECTA
+        if (flag) {
+            Toast.makeText(this, R.string.citaOK,
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            Toast.makeText(this, R.string.falloCita,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+    public void modificarCita() {
+        boolean flag = false;
+
+        // PREPARO LA MODIFICACION
+        adbCitas = new AdaptadorDBCitas(ctx);
+        cv = new ContentValues();
+        cv.put("fecha", fecha);
+        cv.put("hora", hora);
+        cv.put("precio", precio);
+        cv.put("descripcion", descripcion);
+        cv.put("id_mascota", id_mascota);
+
+        // MODIFICO LA CITA
+        try {
+            adbCitas.abrirConexion();
+            adbCitas.modificarCita(id_cita, cv);
+            adbCitas.cerrarConexion();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            finish();
+        }
+    }
+    /**
     public void insertarCita() {
         boolean flag = false;
 
@@ -212,7 +269,7 @@ public class InsertarCita extends ActionBarActivity {
                         descripcion = "No introducida";
                     }
 
-                    // PREPARO LA INSERCION
+                    // PREPARO LA MODIFICACION
                     adbCitas = new AdaptadorDBCitas(ctx);
                     cv = new ContentValues();
                     cv.put("fecha", fecha);
@@ -221,7 +278,7 @@ public class InsertarCita extends ActionBarActivity {
                     cv.put("descripcion", descripcion);
                     cv.put("id_mascota", id_mascota);
 
-                    // INSERTO LA CITA (TRUE -> OK | FALSE -> ERROR AL INSERTAR)
+                    // MODIFICO LA CITA (TRUE -> OK | FALSE -> ERROR AL INSERTAR)
                     try {
                         adbCitas.abrirConexion();
                         adbCitas.modificarCita(id_cita, cv);
@@ -244,9 +301,42 @@ public class InsertarCita extends ActionBarActivity {
                     Toast.LENGTH_SHORT).show();
         }
     }
+*/
+    private boolean comprobarDatos() {
+        boolean flag = false;
+        this.fecha = etFecha.getText().toString();
+        this.hora = etHora.getText().toString();
+        this.precio = etPrecio.getText().toString();
+        this.descripcion = etDescripcion.getText().toString();
+
+        if (comprobarFecha(this.fecha)) {
+            if (comprobarHora(this.hora)) {
+                if (this.precio.length() != 0) {
+                    if (this.descripcion.length() != 0) {
+                        flag = true;
+                    } else {
+                        this.descripcion = "No introducida";
+                    }
+                } else {
+                    flag = false;
+                    Toast.makeText(this, R.string.precioVacio,
+                        Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                flag = false;
+                Toast.makeText(this, R.string.horaIncorrecta,
+                    Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            flag = false;
+            Toast.makeText(this, R.string.fechaIncorrecta,
+                Toast.LENGTH_SHORT).show();
+        }
+        return flag;
+    }
 
     // FUNCION PARA COMPROBAR SI SE HA INTRODUCIDO CORRECTAMENTE LA FECHA
-    private boolean comprobarFecha(String fch) {
+    private boolean comprobarFecha(String f) {
         SimpleDateFormat formatoFecha = null;
         boolean flagFecha = true;
 
@@ -254,7 +344,7 @@ public class InsertarCita extends ActionBarActivity {
             // CONVIERTO LA FECHA AL FORMATO SIMPLE
             formatoFecha = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
             formatoFecha.setLenient(false);
-            formatoFecha.parse(fch);
+            formatoFecha.parse(f);
         } catch (ParseException e) {
             // SI FALLA LA CONVERSION ES PORQUE NO SE HA INTRODUCIDO CORRECTAMENTE LA FECHA
            flagFecha = false;
@@ -264,14 +354,15 @@ public class InsertarCita extends ActionBarActivity {
     }
 
     // FUNCION PARA COMPROBAR SI SE HA INTRODUCIDO CORRECTAMENTE LA HORA
-    private boolean comprobarHora(String hr) {
+    private boolean comprobarHora(String h) {
         SimpleDateFormat formatoHora = null;
         boolean flagHora = true;
 
         try {
             // CONVIERTO LA FECHA AL FORMATO SIMPLE
             formatoHora = new SimpleDateFormat("HH:mm", Locale.getDefault());
-            formatoHora.parse(hr);
+            formatoHora.setLenient(false);
+            formatoHora.parse(h);
         } catch (ParseException e) {
             // SI FALLA LA CONVERSION ES PORQUE NO SE HA INTRODUCIDO CORRECTAMENTE LA FECHA
             flagHora = false;
